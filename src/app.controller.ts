0924873e9be335189b8718d060169c8eb2b26037
postlude <seungdeokhan@naver.com>
2023-07-26 18:47:30 +0900

    change url

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,9 +3,9 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	constructor(private readonly appService: AppService) { }
 
-	@Get('/')
+	@Get('/health')
 	public health() {
 		console.log('call health');
 		return 'ok';
